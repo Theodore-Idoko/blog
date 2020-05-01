59f89bd589c70cf9a83d20b906fe8e50be2c5016
Tochukwu Idoko <idokotheodore@gmail.com>
2020-05-01 16:50:54 +0100

    last commit

---
+++ src/components/UserHeader.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPostsAndUsers } from '../actions';
+
+class UserHeader extends React.Component {
+  componentDidMount() {
+    this.props.fetchPostsAndUsers(this.props.userId)
+  }
+  render() {
+  const { user } = this.props
+    
+    if (!user) {
+      return null;
+    }
+  return <div className='header'>{user.name}</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) };
+}
+
+export default connect(mapStateToProps, { fetchPostsAndUsers }) (UserHeader);
